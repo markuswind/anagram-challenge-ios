0eb4ded204d1ac40f9ab5d28c1b768a613f45b2d
Markus Wind <markuswind@gmail.com>
2017-11-28 11:24:26 +0100

    added styleconstants

---
+++ anagram-challenge-ios/Helpers/StyleConstants.swift
@@ -0,0 +1,27 @@
+//
+//  StyleConstants.swift
+//  anagram-challenge-ios
+//
+//  Created by Markus Wind on 28/11/2017.
+//  Copyright © 2017 Markus Wind. All rights reserved.
+//
+
+import UIKit
+
+struct StyleConstants {
+
+  struct margins {
+    static let tiny: CGFloat = 4
+    static let small: CGFloat = 8
+    static let medium: CGFloat = 16
+    static let large: CGFloat = 32
+  }
+
+  struct colors {
+    static let primary = UIColor(red: 0.81, green: 0.16, blue: 0.18, alpha: 1.00)
+    static let secondary = UIColor.gray
+    static let background = UIColor.white
+    static let navigationBarTextTintColor = UIColor.white
+  }
+
+}
