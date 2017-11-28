ad4496f905225a23107fb689b608f14eb32c1cb2
Markus Wind <markuswind@gmail.com>
2017-11-28 11:17:38 +0100

    Initial Commit

---
+++ anagram-challenge-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  anagram-challenge-ios
+//
+//  Created by Markus Wind on 28/11/2017.
+//  Copyright © 2017 Markus Wind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
